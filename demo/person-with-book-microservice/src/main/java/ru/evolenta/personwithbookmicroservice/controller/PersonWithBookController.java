43263a3bde99446f9a84736b44c81cde8c5b72cd
yushin <d.ushin@evolenta.ru>
2023-01-31 15:00:45 +0300

    add instance id show in controller

--- demo/person-with-book-microservice/src/main/java/ru/evolenta/personwithbookmicroservice/controller/PersonWithBookController.java
+++ demo/person-with-book-microservice/src/main/java/ru/evolenta/personwithbookmicroservice/controller/PersonWithBookController.java
@@ -1,6 +1,7 @@
 package ru.evolenta.personwithbookmicroservice.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,11 +19,19 @@ public class PersonWithBookController {
     @Autowired
     public RestTemplate restTemplate;
 
+    @Value("${eureka.instance.instance-id}")
+    private String instanceId;
+
     @GetMapping("/{personId}/{bookId}")
     public ResponseEntity<Person> reader(@PathVariable int personId, @PathVariable int bookId) {
         Person person = restTemplate.getForObject("http://person-microservice/person/" + personId, Person.class);
         Book book = restTemplate.getForObject("http://book-microservice/book/" + bookId, Book.class);
         person.setBook(book);
         return new ResponseEntity<>(person, HttpStatus.OK);
+    }
+
+    @GetMapping("/test")
+    public String test() {
+        return instanceId;
     }
 }
